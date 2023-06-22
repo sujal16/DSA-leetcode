3ab47d5676535556c5a373b48716a15d4e74b185
sujal16 <86117482+sujal16@users.noreply.github.com>
2023-06-22 12:14:16 +0530

    Add files via upload

---
+++ Leaf-Similar Trees_872.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void traverse(TreeNode* root,vector<int>& ans){
+        if(root==NULL){
+            return;
+        }
+        if(root->right==NULL && root->left==NULL){
+            ans.push_back(root->val);
+            return;
+        }
+        traverse(root->left,ans);
+        traverse(root->right,ans);
+    }
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        vector <int> ans1;
+        vector <int> ans2;
+        traverse(root1,ans1);
+        traverse(root2,ans2);
+        if(ans1.size()!=ans2.size()){
+            return false;
+        }
+        else{
+            for(int i=0;i<ans1.size();i++){
+                if(ans1[i]!=ans2[i]){
+                    return false;
+                }
+            }
+        } return true;
+    }
+};
