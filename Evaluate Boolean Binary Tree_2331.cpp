ad2144c7c2b670a59f9222f04591512e8d9b26e8
sujal16 <86117482+sujal16@users.noreply.github.com>
2023-06-23 09:50:26 +0530

    Add files via upload

---
+++ Evaluate Boolean Binary Tree_2331.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool evaluateTree(TreeNode* root) {
+        if(root==NULL){
+            return true; 
+        }
+        if(root->val==1){
+            return true;
+        }
+        if(root->val==0){
+            return false;
+        }
+        bool ans1 = evaluateTree(root->left);
+        bool ans2 = evaluateTree(root->right);
+        if(root->val==2){
+            return ans1 || ans2;
+        }
+        else{
+            return ans1 && ans2;
+        }
+    }
+};
