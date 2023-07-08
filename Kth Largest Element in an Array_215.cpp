c00b7a215495b75e950ed945f10b34441713f809
sujal16 <86117482+sujal16@users.noreply.github.com>
2023-07-08 09:51:03 +0530

    Add files via upload

---
+++ Kth Largest Element in an Array_215.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue <int> q;
+        for(int i=0;i<nums.size();i++){
+            q.push(nums[i]);
+        }
+        while(k!=1){
+            q.pop();
+            k--;
+        }
+        int ans = q.top();
+        return ans;
+    }
+};
+
