b339e4de1ccaf522ee6160adc1a628e1ddcfcb7a
Sujal Varshney <86117482+sujal16@users.noreply.github.com>
2023-07-18 10:12:23 +0530

    Add files via upload

---
+++ Find the Score of All Prefixes of an Array_2640.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<long long> findPrefixScore(vector<int>& nums) {
+        vector <long long> ans;
+        int maxi = 0;
+        long long sum = 0;
+        for(int i=0;i<nums.size();i++){
+            maxi = max(maxi,nums[i]);
+            sum += nums[i] + maxi ;
+            ans.push_back(sum);
+        }
+        return ans;
+    }
+};
+
+
